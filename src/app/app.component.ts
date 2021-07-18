af11806d99f2d16944205867b9ed6e76abb7b135
irwanbekasi1993 <irwanbekasi1993@gmail.com>
2021-07-18 16:15:51 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TokenStorageService } from './services/services/token-storage.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular11jwtauth';
+  private roles: string[] = [];
+  isLoggedIn = false;
+  showAdminBoard = false;
+  showModeratorBoard = false;
+  username?: string;
+
+  constructor(private tokenStorageService: TokenStorageService){}
+
+  ngOnInit(): void {
+
+     //this.isLoggedIn = !this.tokenStorageService.getToken();
+     this.isLoggedIn = true;
+     if (this.isLoggedIn){
+       const user = this.tokenStorageService.getUser();
+       this.roles = user.roles;
+
+       //this.showAdminBoard = this.roles.includes('ROLE_ADMIN');
+       //this.showModeratorBoard = this.roles.includes('ROLE_MODERATOR');
+       this.username = user.username;
+
+     }
+
+
+  }
+
+  logout(): void{
+    this.tokenStorageService.signOut();
+    this.isLoggedIn = false;
+    window.location.reload();
+  }
+
 }
